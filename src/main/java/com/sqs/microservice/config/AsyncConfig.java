8d7376957d95cc7959499655162e4536e8be8016
Winicius Souza <winiciussouzadev@gmail.com>
2024-11-28 19:25:34 -0300

    feat: classes e configuracoes iniciais

---
+++ src/main/java/com/sqs/microservice/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.sqs.microservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-");
+        executor.initialize();
+        return executor;
+    }
+
+}
---
+++ src/main/java/com/sqs/microservice/domain/SqsProperties.java
@@ -0,0 +1,16 @@
+package com.sqs.microservice.domain;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "aws.sqs.localstack")
+@Data
+public class SqsProperties {
+
+    private String uri;
+    private String url;
+
+}
